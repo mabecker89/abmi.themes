c2b1b93fd69b141583f1e01712c3652b0dd2282c
Marcus Becker <mabecker@ualberta.ca>
2020-05-06 10:30:04 -0600

    add alternative logos, update add_logo fn

--- R/add_logo.R
+++ R/add_logo.R
@@ -1,8 +1,14 @@
 #' Add ABMI logo to the bottom of the plot
 #'
 #' @param plot_name The variable name of the plot you have created
-#' @param logo_image_path File path for the logo image you want to use in the right hand side of your plot, which needs to be a PNG file; defaults to the ABMI's primary horizontal acronym logo that is stored in the data\ folder of this package
+#' @param logo Character; Specify which logo you want (see details below)
 #' @param logo_position A numeric vector or unit object specifying x-location of logo; defaults to 0.925 (right justified)
+#' @details Valid values for the \code{logo} argument currently are:
+#' \itemize{
+#' \item "acronym" (default)
+#' \item "full"
+#' \item Personal file path for a logo image you want to use (needs to be a png file)
+#' }
 #' @importFrom grid grobTree rasterGrob
 #' @importFrom png readPNG
 #' @importFrom ggpubr ggarrange
@@ -11,12 +17,25 @@
 #'
 #' @examples
 #' \donttest{
-#' add_logo(p)
+#' add_logo(p, logo = "full", logo_position = 0.875)
 #' }
 
 add_logo <- function (plot_name,
-                      logo_image_path = file.path(system.file("extdata", package = 'abmi.themes'), "horizontal-logo-acronym.PNG"),
+                      logo = "acronym",
                       logo_position = 0.925) {
+
+  # Logo choices
+  if(logo == "acronym") {
+    logo_image_path = file.path(system.file("extdata", package = 'abmi.themes'), "ABMI-Horizontal-FullColour-acronym.png")
+  } else if(logo == "full") {
+    logo_image_path = file.path(system.file("extdata", package = 'abmi.themes'), "ABMI-Horizontal-FullColour.png")
+  } else {
+    logo_image_path = logo
+  }
+
+  if(logo == "full" & logo_position >= 0.900) {
+    message("With the current position value, your logo will be partially cut off. If you want it in the bottom right, a good default position when using the full logo is 0.875")
+  }
 
   # Make footer
   footer <- grid::grobTree(grid::rasterGrob(png::readPNG(logo_image_path), x = logo_position))
